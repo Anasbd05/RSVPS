ecb58348c2c6880bbc0c884d07254d04b969153e
anas bd <anastrying05@gmail.com>
2025-03-05 17:32:00 +0000

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Outfit} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const outfit = Outfit({
   subsets: ["latin"],
 });
 
@@ -24,8 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body cz-shortcut-listen="true"
+        className={`${outfit.className} py-3 bg-[#222831] text-[#EEEEEE]  antialiased`}
       >
         {children}
       </body>
